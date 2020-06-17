376b1a10429b07304b885cbe6cc0eb12e8b811f1
fuchsroey <fuchsroey@gmail.com>
2020-06-17 20:02:46 +0300

    add support in cli add option to output to file add comments

--- Main.java
+++ Main.java
@@ -1,28 +1,60 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.io.File;
+import java.io.PrintWriter;
+
+import org.apache.commons.cli.*;
+
 
 public class Main {
+    private static final String XML_FILE_FLGA = "x";
+    private static final String OUTPUT_FILE_FLAG = "o";
+
     public static void main(String[] args) {
 
+        //cli parse
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd;
+        try {
+            cmd = parser.parse(Main.getCLIOptions(), args);
+        } catch (Exception e) {
+            System.err.println("Error while parse CLI");
+            System.err.println(e.getMessage());
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp(" ", Main.getCLIOptions());
+            return;
+        }
+        //check if xml file was set
+        if (!cmd.hasOption(XML_FILE_FLGA)) {
+            System.err.println("XML File didn't set");
+            return;
+        }
 
-
-        var a=LoggerParser.GetReports("test2.xml");
+        var a = LoggerParser.GetReports(cmd.getOptionValue(XML_FILE_FLGA));
         var b = new InteresPointFinder(a);
         var s = b.getInteresPoints();
 
-        for (Report r:a) {
-            System.out.println("Agent Loc: ("+r.getAgentLocation().getX()
-                    +","+r.getAgentLocation().getY()+")");
-            System.out.println("Points chacked:");
-            for (Point p:r.GetPointsChecked()){
-                System.out.println(p);
+        PrintWriter pr;
+        //if output file didn't set - use console.
+        if (cmd.hasOption(OUTPUT_FILE_FLAG)){
+            try {
+                pr = new PrintWriter(new File(cmd.getOptionValue(OUTPUT_FILE_FLAG)));
+            } catch (Exception e){
+                System.err.println("Error when create output file");
+                return;
             }
-            System.out.println("Points compared:");
-            for (Pair<Point,Point> p:r.GetPointsCompared()){
-                System.out.println(p.getLeft()+" "+p.getRight());
-            }
-
+        } else {
+            pr = new PrintWriter(System.out);
         }
+        for (var p:s) {
+            pr.println(p.toString());
+        }
+        pr.close();
+    }
 
+    //CLI Options
+    private static Options getCLIOptions() {
+        Options options = new Options();
+        options.addOption(OptionBuilder.withArgName("file").hasArg().withDescription("xml file").create(XML_FILE_FLGA));
+        options.addOption(OptionBuilder.withArgName("file").hasArg().withDescription("output file. if didn't set - use console").create(OUTPUT_FILE_FLAG));
+        return options;
     }
 }
