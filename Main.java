1178aa69064f64e0195530fa34e91b93c1c2595a
fuchsroey <fuchsroey@gmail.com>
2020-06-19 17:29:58 +0300

    add help arg

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ import org.apache.commons.cli.*;
 public class Main {
     private static final String XML_FILE_FLAG = "x";
     private static final String OUTPUT_FILE_FLAG = "o";
+    private static final String HELP_FLAG = "h";
 
     public static void main(String[] args) {
 
@@ -18,6 +19,11 @@ public class Main {
         } catch (Exception e) {
             System.err.println("Error while parse CLI");
             System.err.println(e.getMessage());
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp(" ", Main.getCLIOptions());
+            return;
+        }
+        if(cmd.hasOption(HELP_FLAG)) {
             HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp(" ", Main.getCLIOptions());
             return;
@@ -34,17 +40,17 @@ public class Main {
 
         PrintWriter pr;
         //if output file didn't set - use console.
-        if (cmd.hasOption(OUTPUT_FILE_FLAG)){
+        if (cmd.hasOption(OUTPUT_FILE_FLAG)) {
             try {
                 pr = new PrintWriter(new File(cmd.getOptionValue(OUTPUT_FILE_FLAG)));
-            } catch (Exception e){
+            } catch (Exception e) {
                 System.err.println("Error when create output file");
                 return;
             }
         } else {
             pr = new PrintWriter(System.out);
         }
-        for (var p:s) {
+        for (var p : s) {
             pr.println(p.toString());
         }
         pr.close();
@@ -55,6 +61,7 @@ public class Main {
         Options options = new Options();
         options.addOption(Option.builder(OUTPUT_FILE_FLAG).hasArg().argName("file").desc("output file. if didn't set - use console").build());
         options.addOption(Option.builder(XML_FILE_FLAG).hasArg().argName("file").desc("xml file").build());
+        options.addOption(Option.builder(HELP_FLAG).desc("print this message").build());
         return options;
     }
 }
