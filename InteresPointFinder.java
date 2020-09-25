49e735d71fcc95704b6aa5e4d04ad0abb5c81577
Stav Koren <stavkoren123@gmail.com>
2020-09-25 17:20:23 +0300

    InteresPointFinder: update reports

--- InteresPointFinder.java
+++ InteresPointFinder.java
@@ -11,16 +11,9 @@ public class InteresPointFinder {
     }
 
     public List<Report> updateReports() {
-        ArrayList<Report> reports = new ArrayList<>();
-        reports.addAll(this.methods.get(0).updateReports());
-        for (int i=1;i<this.methods.size(); i++){
-            List<Report> newReports=methods.get(i).updateReports();
-            for (int r=0; r<reports.size();r++){
-                if (reports.get(i).getInterestingPoint()==false
-                && newReports.get(i).getInterestingPoint()==true){
-                    reports.set(i,newReports.get(i));
-                }
-            }
+        List<Report> reports = new ArrayList<>();
+        for (int i=0;i<this.methods.size(); i++){
+            reports= this.methods.get(i).updateReports();
         }
         return reports;
     }
